80edfaee625b09d6731ff80f191a6069cbdc3c17
amin <med.amin.chniti@gmail.com>
2026-02-19 05:01:47 -1200

    changment au jenkisfiless and auth

--- backend/src/auth/auth/auth.service.spec.ts
+++ backend/src/auth/auth/auth.service.spec.ts
@@ -1,18 +1,34 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { AuthService } from './auth.service';
+import { Test, TestingModule } from '@nestjs/testing'
+import { JwtService } from '@nestjs/jwt'
+import { AuthService } from './auth.service'
+import { UsersService } from '../../users/users.service'
 
 describe('AuthService', () => {
-  let service: AuthService;
+  let service: AuthService
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
-    }).compile();
+      providers: [
+        AuthService,
+        {
+          provide: UsersService,
+          useValue: {
+            findByEmail: jest.fn(),
+          },
+        },
+        {
+          provide: JwtService,
+          useValue: {
+            sign: jest.fn(),
+          },
+        },
+      ],
+    }).compile()
 
-    service = module.get<AuthService>(AuthService);
-  });
+    service = module.get<AuthService>(AuthService)
+  })
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
-});
+    expect(service).toBeDefined()
+  })
+})
